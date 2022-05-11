983d9d1a7b6db56748cd0909bc5d9458c1303cb5
moeyg <moooeyg@gmail.com>
2022-05-11 14:30:26 +0900

    Add object note

---
+++ 5.object/04.method.js
@@ -0,0 +1,12 @@
+// 메소드 (Method)
+// key: value 에서 value를 함수로 사용할 수 있다.
+
+// this.keyName : 자기 자신의 속성에 접근하고 싶을 때는 this 사용
+const apple = {
+  name: '사과',
+  display: function () {
+    console.log(`${this.name}: 🍎`);
+  },
+};
+
+apple.display(); // 사과: 🍎
