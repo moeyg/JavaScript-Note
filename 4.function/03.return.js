5a31099536afae46d67672c2f0d7d1c92f45739e
Dogyeom Park <102715022+moeyg@users.noreply.github.com>
2022-06-20 14:50:52 +0900

    Update 03.return.js

--- 4.function/03.return.js
+++ 4.function/03.return.js
@@ -1,5 +1,4 @@
 // return을 명시적으로 하지 않으면 자동으로 undefined이 반환됨!
-
 function add(a, b) {
   // return a + b;
 }
@@ -9,12 +8,12 @@ console.log(result); // undefined
 
 // 예제 : 조건이 맞지 않는 경우 함수 도입 부분에서 함수를 일찍이 종료함!!
 // return을 함수 중간에 하게 되면 함수가 종료됨
-function print(num) {
+function printNumber(num) {
   if (num < 0) {
     return; // = return undefined
   }
   console.log(num);
 }
 
-print(1); // 1
-print(-1); // 출력 ❌
+printNumber(1); // 1
+printNumber(-1); // 출력 ❌
