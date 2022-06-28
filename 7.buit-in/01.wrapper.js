b09cb222c88bb3ba38ddda5d692a29fa75145db7
Dogyeom Park <102715022+moeyg@users.noreply.github.com>
2022-06-28 19:09:12 +0900

    Update 01.wrapper.js

--- 7.buit-in/01.wrapper.js
+++ 7.buit-in/01.wrapper.js
@@ -1,5 +1,5 @@
 // 래퍼 객체 (Wrapper Object)
-// 원시 값을 필요에 따라서 관련된 빌트인 객체로 변환한다.
+// 원시 값을 필요에 따라 관련된 빌트인 객체로 변환한다.
 
 const number = 123; // number 원시 타입
 console.log(number.toString()); // number 원시 타입을 감싸고 있는 Number 라는 클래스인 객체로 감싸지고, toString 함수 사용 가능
