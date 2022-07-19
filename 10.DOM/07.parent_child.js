cf61a9f260947680fba7678b11332f8b32abe7fe
moeyg <moooeyg@gmail.com>
2022-07-19 17:32:11 +0900

    Add 07.parent_child.js

---
+++ 10.DOM/07.parent_child.js
@@ -0,0 +1,6 @@
+// 다음에 오는 형제 요소에 접근
+document.querySelector('h1').nextElementSibling;
+// 이전에 오는 형제 요소에 접근
+document.querySelector('h1').previousElementSibling;
+// 부모 요소에 접근
+document.querySelector('h1').parentElement;
