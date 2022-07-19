f9ef6f1d560640d7a8189982badf42a88933bd56
moeyg <moooeyg@gmail.com>
2022-07-19 21:24:29 +0900

    Add 09.remove

---
+++ 10.DOM/09.remove.js
@@ -0,0 +1,6 @@
+// removeChild -> 부모 요소를 선택한 후 삭제할 수 있다.
+const list = document.querySelector('li');
+list.parentElement.removeChild(list);
+
+// remove -> 요소를 선택해 삭제할 수 있다.
+document.querySelector('img').remove();
