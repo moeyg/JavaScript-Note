1dc6f54f4514461357508c5628928e4a4f1fbe67
moeyg <moooeyg@gmail.com>
2022-07-20 18:15:44 +0900

    Add 06.inputEvent

--- 11.DOM_event/06.inputEvent.js
+++ 11.DOM_event/06.inputEvent.js
@@ -1,6 +1,11 @@
 const h1 = document.querySelector('h1');
 const input = document.querySelector('input');
+const defaultMsg = h1.innerText;
 
-input.addEventListener('input', () => {
-  h1.innerText = input.value;
+input.addEventListener('input', (e) => {
+  if (!e.target.value) {
+    h1.innerText = defaultMsg;
+  } else {
+    h1.innerText = `Welcome, ${e.target.value}`;
+  }
 });
