c32dff95f5a16c397850eff5137d5e1149ac49c1
moeyg <moooeyg@gmail.com>
2022-07-21 16:03:50 +0900

    Add 04-3.promise

---
+++ 12.asynchronous/04-3.promise.js
@@ -0,0 +1,15 @@
+const delayColorChange = (color, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, delay);
+  });
+};
+
+delayColorChange('red', 1000)
+  .then(() => delayColorChange('orange', 1000))
+  .then(() => delayColorChange('yellow', 1000))
+  .then(() => delayColorChange('green', 1000))
+  .then(() => delayColorChange('blue', 1000))
+  .then(() => delayColorChange('purple', 1000));
